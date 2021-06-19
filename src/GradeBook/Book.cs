eab093b8ee23fb487c9b30c819ee2adf51049f86
Capt. Niro <niroshana.pathirana@crownresorts.com.au>
2021-06-19 23:57:22 +1000

    Added for loop

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -13,8 +13,14 @@ namespace GradeBook
 
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
-
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
         }
 
         public Statistics GetStatistics()
@@ -25,12 +31,13 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            for (var index = 0; index < grades.Count;index++)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
             }
+
             result.Average /= grades.Count;
 
             return result;
